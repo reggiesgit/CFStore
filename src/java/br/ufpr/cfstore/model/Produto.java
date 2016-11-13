b7a3134f6ccc580b621158486ac9714ad241006f
Regis Gaboardi <regisgaboardi@gmail.com>
2016-11-13 11:22:45 -0200

    Commit UC03 Produto rev 2.2
    Commit da classe Produto.java

--- src/java/br/ufpr/cfstore/model/Produto.java
+++ src/java/br/ufpr/cfstore/model/Produto.java
@@ -71,11 +71,13 @@ public class Produto {
     /*
     Fim Getters and Setters, Início CRUD de Produtos.
     */
+    
     /**
      * Este metodo busca todos os produtos ou produtos que possuem "fragmento"
      * em seu nome, assim como outros parâmetros relacionados ao mesmo; [fornecedor,
      * categoria, subCategoria]
      * @param fragmento
+     * @param pagina
      * @return 
      */
     public List<Produto> listarProdutos(String fragmento, int pagina) {
@@ -83,10 +85,12 @@ public class Produto {
         Connection conn = null;
         
         try{
+            String sql = "CALL SP0101(?,?,?)";
             conn = DBConnector.getConnection();
             CallableStatement stmt = conn.prepareCall(sql);
             stmt.setString(1, fragmento);
             stmt.setInt(2, pagina);
+            stmt.setInt(3, 0);
             ResultSet rs = stmt.executeQuery();
             while(rs.next()) {
                 Produto dbProduto = new Produto();
@@ -107,9 +111,11 @@ public class Produto {
     
     
     /**
+     * Este metodo busca as recomendacoes pelo id do produto
      * @param idProduto
      * @return 
      */
+    public List<Produto> buscarRecomendacoes(int idProduto) {
         Connection conn = null;
         List<Produto> recomendacoes = new ArrayList();
         String sql = "SELECT a.itemRecomendado as idProduto, b.produto, b.precoVenda from recomendacao a inner join \n" +
@@ -133,9 +139,99 @@ public class Produto {
         return recomendacoes;
     }
      /**
+     * Este metodo busca um produto pelo id
      * @param idProduto
      * @return 
      */
+    public Produto retornaProduto(int idProduto) {
+        Produto dbProduto = new Produto();
         Connection conn = null;
+        try{
+            String sql = "CALL SP0101(?,?,?)";
+            conn = DBConnector.getConnection();
+            CallableStatement stmt = conn.prepareCall(sql);
+            stmt.setString(1, "");
+            stmt.setInt(2, 1);
+            stmt.setInt(3, idProduto);
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+            dbProduto.setId(Integer.parseInt(rs.getString("idProduto")));
+            dbProduto.setNome(rs.getString("produto"));
+            dbProduto.setPrecoUnitario(rs.getDouble("precoReal"));
+            return dbProduto;  
+        } catch (SQLException sqle) {
+            System.out.println("Erro ao efetuar a busca no banco de dados: " + sqle.getMessage());
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Produto.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    return dbProduto;
+    }
+    
+    /**
+     * Este metodo adiciona um item no carrinho.
+     * O carrinho é identificado pelo ID da sessão se não houver um login ativo.
+     * Quando o cliente faz login, o ID da sessão é atrelado ao seu ID.
+     * @param sessionID
+     * @param clienteID
+     * @param idToAdd
+     * @param unidades
+     * @return 
+     */
+    public List<String> adicionarNoCarrinho(String sessionID, int clienteID, int idToAdd, int unidades) {
+        Connection conn = null;
+        List<String> retorno = new ArrayList<>();
+        try{
+            String sql = "CALL SP0303(?,?,?,?)";
+            conn = DBConnector.getConnection();
+            CallableStatement stmt = conn.prepareCall(sql);
+            stmt.setInt(1, 1);
+            stmt.setString(2, sessionID);
+            stmt.setInt(3, clienteID);
+            stmt.setInt(4, idToAdd);
+            stmt.setInt(5, unidades);
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+            retorno.add(rs.getString("out_cod"));
+            retorno.add(rs.getString("out_msg"));
+        } catch (SQLException sqle) {
+            System.out.println("Erro ao efetuar a busca no banco de dados: " + sqle.getMessage());
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Produto.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return retorno;
+    }
+    
+    /**
+     * Este metodo remove itens do carrinho a partir do ID do item.
+     * O carrinho é identificado pelo ID da sessão se não houver um login ativo.
+     * Quando o cliente faz login, o ID da sessão é atrelado ao seu ID.
+     * @param sessionID
+     * @param clienteID
+     * @param idToAdd
+     * @param unidades
+     * @return 
+     */
+    public List<String> removerDoCarrinho(String sessionID, int clienteID, int idToAdd, int unidades) {
+        Connection conn = null;
+        List<String> retorno = new ArrayList<>();
+        try{
+            String sql = "CALL SP0303(?,?,?,?)";
+            conn = DBConnector.getConnection();
+            CallableStatement stmt = conn.prepareCall(sql);
+            stmt.setInt(1, 3);
+            stmt.setString(2, sessionID);
+            stmt.setInt(3, clienteID);
+            stmt.setInt(4, idToAdd);
+            stmt.setInt(5, unidades);
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+            retorno.add(rs.getString("out_cod"));
+            retorno.add(rs.getString("out_msg"));
+        } catch (SQLException sqle) {
+            System.out.println("Erro ao efetuar a busca no banco de dados: " + sqle.getMessage());
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Produto.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return retorno;
     }
 }
